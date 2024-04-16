0e37f38f638b33f8d5fda47290fe4bd6d58b4cb5
DasminX <wojtek.test99@gmail.com>
2024-04-16 20:36:45 +0200

    feat: start refactor whole page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { NavbarLinks } from "./NavbarLinks";
 import { NavbarSocials } from "./NavbarSocials";
 import { NavbarToggle } from "./NavbarToggle";
+import { SourcePicture } from "../_shared/SourcePicture";
 
 export const Navbar = () => {
   const [isActive, setIsActive] = useState(false);
@@ -13,11 +14,19 @@ export const Navbar = () => {
 
   return (
     <nav className={`nav ${isActive ? "nav__active" : ""}`}>
-      <NavbarToggle onClick={navbarToggleHandler} isActive={isActive} />
-      <ul className="nav__list">
-        <NavbarLinks onClick={navbarToggleHandler} />
-        <NavbarSocials onClick={navbarToggleHandler} />
-      </ul>
+      {/* <NavbarToggle onClick={navbarToggleHandler} isActive={isActive} /> */}
+      <div className="nav__inner">
+        <div className="logo">
+          <SourcePicture
+            path="logo_dd.png"
+            alt="Logo D&D Bezpieczna Przystań"
+          />
+        </div>
+        <ul className="nav__list">
+          <NavbarLinks onClick={navbarToggleHandler} />
+          <NavbarSocials onClick={navbarToggleHandler} />
+        </ul>
+      </div>
     </nav>
   );
 };
